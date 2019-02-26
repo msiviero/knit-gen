efa149e2af372acb7088f40fb9baa1b8138dc6b7
msiviero <m.siviero83@gmail.com>
2019-02-26 23:32:35 +0100

    Added basic template

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 #!/usr/bin/env node
+import { promises as fs } from "fs";
+import * as inquirer from "inquirer";
+import { tplDirectory } from "./constants";
 
-export const main = async () => {
-    // tslint:disable-next-line:no-console
-    console.log("Hello world");
+interface ProjectAnswers {
+    template: string;
+    name: string;
+}
+
+const validateProjectName = (input: string) => /^([A-Za-z\-\_\d])+$/.test(input)
+    ? true
+    : "Project name may only include letters, numbers, underscores and hashes.";
+
+export const runner = async () => {
+
+    const templates = await fs.readdir(tplDirectory);
+
+    inquirer
+        .prompt<ProjectAnswers>([{
+            name: "template",
+            type: "list",
+            message: "What type of would you like to generate?",
+            choices: templates,
+        }, {
+            name: "name",
+            type: "input",
+            message: "Project name:",
+            validate: validateProjectName,
+        }])
+        .then((answers) => {
+            console.log(answers);
+        });
 };
 
-main();
+runner();
